a0959333692650df2f2722bd49d35c54d129c2d8
Girko <girko131@gmail.com>
2023-05-28 12:51:55 +0300

    Finish Task number 2

---
+++ About.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace Girko_IKM_721A_cource_project
+{
+    partial class About
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tAbout = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(390, 600);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(155, 57);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Закрити";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(347, 348);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(255, 150);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Розробив\r\nСтудент групи ІКМ-721А\r\nГірко Д.\r\n\r\nТекст завдання:\r\nТекст";
+            // 
+            // tAbout
+            // 
+            this.tAbout.Interval = 10000;
+            this.tAbout.Tick += new System.EventHandler(this.tAbout_Tick);
+            // 
+            // About
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1062, 1022);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "About";
+            this.Text = "About";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        public System.Windows.Forms.Timer tAbout;
+    }
+}
