3d1b5eac9a9d4853405fc7c6d5ef737d6112e717
Girko <girko131@gmail.com>
2023-05-28 13:14:46 +0300

    Finish Task number 6

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -97,7 +97,7 @@
             this.довідкаToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(1382, 42);
+            this.menuStrip1.Size = new System.Drawing.Size(1382, 40);
             this.menuStrip1.TabIndex = 3;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -140,6 +140,7 @@
             this.новийToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.N)));
             this.новийToolStripMenuItem.Size = new System.Drawing.Size(359, 44);
             this.новийToolStripMenuItem.Text = "Новий ";
+            this.новийToolStripMenuItem.Click += new System.EventHandler(this.новийToolStripMenuItem_Click);
             // 
             // відкритиToolStripMenuItem
             // 
@@ -155,6 +156,7 @@
             this.зберегтиToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.S)));
             this.зберегтиToolStripMenuItem.Size = new System.Drawing.Size(359, 44);
             this.зберегтиToolStripMenuItem.Text = "Зберегти";
+            this.зберегтиToolStripMenuItem.Click += new System.EventHandler(this.зберегтиToolStripMenuItem_Click);
             // 
             // зберегтиЯкToolStripMenuItem
             // 
@@ -213,9 +215,14 @@
             this.toolStripSeparator4.Name = "toolStripSeparator4";
             this.toolStripSeparator4.Size = new System.Drawing.Size(356, 6);
             // 
+            // SfdSave
+            // 
+            this.SfdSave.Filter = "|*.SoM|All files|*.*";
+            // 
             // OfdOpen
             // 
             this.OfdOpen.FileName = "openFileDialog1";
+            this.OfdOpen.Filter = "|*.SoM|All files|*.*";
             // 
             // Form1
             // 
@@ -229,6 +236,7 @@
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
             this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
             this.Load += new System.EventHandler(this.Form1_Load);
             this.menuStrip1.ResumeLayout(false);
